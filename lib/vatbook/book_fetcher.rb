62d31e60fadffd94cf33358f2d11a62b580b4258
rzimin <RZimin@gmail.com>
2013-11-13 23:29:41 +0300

    Trying to work with local xml

--- lib/vatbook/book_fetcher.rb
+++ lib/vatbook/book_fetcher.rb
@@ -1,8 +1,14 @@
 module Vatbook
 
   class BookFetcher
+    %w{curb tempfile time_diff tmpdir csv}.each { |lib| require lib }
     require 'nokogiri'
     require 'open-uri'
+
+    STATUS_URL = "http://status.vatsim.net/status.txt"
+    LOCAL_STATUS = "#{Dir.tmpdir}/vatsim_status.txt"
+    LOCAL_DATA = "#{Dir.tmpdir}/vatbook.xml"
+
 
     attr_accessor :fir, :atc_bookings, :pilot_bookings, :enroute, :doc
 
@@ -11,7 +17,7 @@ module Vatbook
       @enroute = true
       process_arguments(args) if args.class == Hash
       @atc_bookings = []; @pilot_bookings = []; @atc_rus_bookings = []
-      @doc = raw_list
+      fir == '' ? @doc = data_file : @doc = raw_list
       atcs
       atcs_rus
       pilots
@@ -75,6 +81,40 @@ module Vatbook
 
     def process_arguments(args)
       args[:enroute] == false ? @enroute = false : @enroute = true
+    end
+
+   def data_file      
+      File.exists?(LOCAL_DATA) ? read_local_datafile : create_local_data_file
+      LOCAL_DATA    
+    end
+   
+    def read_local_datafile
+      data = File.open(LOCAL_DATA)
+      difference = Time.diff(data.ctime, Time.now)[:minute]
+      difference > 2 ? create_local_data_file : data.read
+    end
+
+    def create_local_data_file
+          curl = Curl::Easy.new('http://vatbook.euroutepro.com/xml2.php')
+          curl.timeout = 5
+          curl.perform
+          curl = curl.body_str
+          data = Tempfile.new('vatbook.xml', :encoding => 'utf-8')
+          File.rename data.path, LOCAL_DATA
+          data = curl.gsub(/["]/, '\s').encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '').encode!('UTF-8', 'UTF-16')
+          data = data.slice(0..(data.index('!PREFILE:')))
+          File.open(LOCAL_DATA, "w+") {|f| f.write(data)}
+          File.chmod(0777, LOCAL_DATA)
+          gem_data_file if curl.include? "<html><head>"
+          gem_data_file if File.open(LOCAL_DATA).size == 0
+        rescue Curl::Err::HostResolutionError
+          gem_data_file
+        rescue Curl::Err::TimeoutError
+          gem_data_file
+        rescue
+          gem_data_file
+        rescue Exception
+          gem_data_file    
     end
 
   end
